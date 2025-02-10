464d116f0571559151650351e2a8d02ff5afbcda
AnhDuong2312 <pad23122002@gmail.com>
2025-02-11 01:34:48 +0700

    Refactor project structure

---
+++ src/Theme.js
@@ -0,0 +1,27 @@
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { cyan, teal, deepOrange, orange } from '@mui/material/colors'
+
+const theme = extendTheme({
+    trello: {
+        appBarHeight: '48px',
+        boardBarHeight: '58px'
+    },
+
+    colorSchemes: {
+        light: {
+            palette: {
+                primary: teal,
+                secondary: deepOrange
+            }
+        },
+
+        dark: {
+            palette: {
+                primary: cyan,
+                secondary: orange
+            }
+        }
+    }
+})
+
+export default theme;
